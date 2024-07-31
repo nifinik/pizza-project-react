eda25f040fb4406b276cf10862bdaad1cb6aa951
Nurzhigit <Nurjigit00a@gmail.com>
2024-07-31 19:35:03 +0500

    /4 lesson ---- useState, onClick, spread, destrukturization, props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import Categories from "./components/Categories";
+import Header from "./components/Header";
+import PizzaBlock from "./components/PizzaBlock";
+import Sort from "./components/Sort";
+import "./scss/app.scss";
+import pizzas from "./assets/pizzas.json";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <Header />
+      <div className="content">
+        <div className="container">
+          <div className="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 className="content__title">Все пиццы</h2>
+          <div className="content__items">
+            {pizzas.map((obj) => {
+              return (
+                <PizzaBlock
+                  // {...obj} Можно сделать вот так она перебирает все своиства в обьекте pizzas но имена своиства должны быть схожими как на обьекте
+                  title={obj.title}
+                  price={obj.price}
+                  imageUrl={obj.imageUrl}
+                  sizes={obj.sizes}
+                  types={obj.types}
+                />
+              );
+            })}
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/components/Categories.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+
+const Categories = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
+
+  const onClickCategory = (index) => {
+    setActiveIndex(index);
+  };
+
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((value, i) => {
+          return (
+            <li
+              onClick={() => onClickCategory(i)}
+              className={activeIndex === i ? "active" : ""}
+            >
+              {value}
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
+};
+
+export default Categories;
